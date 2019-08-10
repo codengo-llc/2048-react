bbe29548894e37e5dbcc4c210ac9b436574ba99e
adamzaczek <adamzaczekul@gmail.com>
2019-08-10 16:34:34 +0200

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,35 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-let prefix = require('gulp-autoprefixer');
-let babel = require('gulp-babel');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const prefix = require("gulp-autoprefixer");
+const babel = require("gulp-babel");
 
-let styles = 'src/*.scss';
-let scripts = 'src/*.js';
+const styles = "src/*.scss";
+const scripts = "src/*.js";
 
-gulp.task('styles', function () {
-  return gulp.src(styles)
+gulp.task("styles", function() {
+  return gulp
+    .src(styles)
     .pipe(sass())
-    .pipe(prefix('last 2 versions', '> 1%', 'ie 8', 'ie 7'))
-    .pipe(gulp.dest('built'));
+    .pipe(prefix("last 2 versions", "> 1%", "ie 8", "ie 7"))
+    .pipe(gulp.dest("built"));
 });
 
-gulp.task('jsx', function () {
-  return gulp.src(scripts)
-    .pipe(babel({
-      presets: ['es2015', 'react']
-    }))
-    .pipe(gulp.dest('built'));
+gulp.task("jsx", function() {
+  return gulp
+    .src(scripts)
+    .pipe(
+      babel({
+        presets: ["es2015", "react"]
+      })
+    )
+    .pipe(gulp.dest("built"));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(styles, ['styles']);
-  gulp.watch(scripts, ['jsx']);
+gulp.task("watch", function() {
+  gulp.watch(styles, ["styles"]);
+  gulp.watch(scripts, ["jsx"]);
 });
 
-gulp.task('default', ['watch', 'styles', 'jsx']);
+gulp.task("default", ["watch", "styles", "jsx"]);
 
-gulp.task('build', ['styles', 'jsx']);
+gulp.task("build", ["styles", "jsx"]);
